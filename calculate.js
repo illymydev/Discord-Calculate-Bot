0dc4b5212353b8ee2445425ae1b39fa8814debe2
Kyohyun Gu <illydev@proton.me>
2023-04-09 13:15:04 +0900

    오류수정

--- calculate.js
+++ calculate.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('messageCreate', msg => {
   if (msg.content.startsWith('!calculate')) {
-    const expression = msg.content.slice(11); // remove the "!calculate " prefix
+    const expression = msg.content.slice(11);
     const result = eval(expression);
-    msg.reply(result);
+    msg.reply(result.toString());
   }
 });
 
